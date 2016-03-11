0e388b6bb12954b5e185eaff850d0d3f3bef6498
Jonathan Svärdén <jonathan.svarden@softhouse.se>
2016-03-11 11:06:31 +0100

    Init

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <div>
+      <pace :seconds="seconds" unit="km"></pace>
+      <span>equals</span>
+      <pace :seconds="seconds" unit="mile"></pace>
+    </div>
+    <distance-result :seconds="seconds" distance="5" distance-name="5k"></distance-result>
+    <distance-result :seconds="seconds" distance="10" distance-name="10k"></distance-result>
+    <distance-result :seconds="seconds" distance="21.098" distance-name="Half marathon"></distance-result>
+    <distance-result :seconds="seconds" distance="42.195" distance-name="Full marathon"></distance-result>
+    <div>
+      <input type="range" min="180" max="540" step="1" v-model="seconds">
+    </div>
+  </div>
+</template>
+
+<script>
+import Pace from './components/Pace'
+import DistanceResult from './components/DistanceResult'
+
+export default {
+  components: {
+    Pace,
+    DistanceResult
+  },
+
+  data () {
+    return {
+      seconds: 360
+    }
+  }
+}
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+#app {
+  margin-top: -100px;
+  max-width: 600px;
+  font-family: Helvetica, sans-serif;
+  text-align: center;
+}
+
+.logo {
+  width: 100px;
+  height: 100px
+}
+
+input[type=range] {
+  width: 100%;
+}
+</style>
