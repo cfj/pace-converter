d98fd002c50c548a8794b9970b3156d9bf3cccd8
Jonathan Svärdén <jonathan.svarden@softhouse.se>
2016-03-11 11:22:25 +0100

    Update style and layout

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <div id="app">
+  <div id="app" class="main">
     <div>
       <pace :seconds="seconds" unit="km"></pace>
       <span>equals</span>
       <pace :seconds="seconds" unit="mile"></pace>
     </div>
-    <distance-result :seconds="seconds" distance="5" distance-name="5k"></distance-result>
-    <distance-result :seconds="seconds" distance="10" distance-name="10k"></distance-result>
-    <distance-result :seconds="seconds" distance="21.098" distance-name="Half marathon"></distance-result>
-    <distance-result :seconds="seconds" distance="42.195" distance-name="Full marathon"></distance-result>
-    <div>
-      <input type="range" min="180" max="540" step="1" v-model="seconds">
+    <input type="range" min="180" max="540" step="1" v-model="seconds">
+    <div class="distance-results">
+      <distance-result :seconds="seconds" distance="5" distance-name="5k"></distance-result>
+      <distance-result :seconds="seconds" distance="10" distance-name="10k"></distance-result>
+      <distance-result :seconds="seconds" distance="21.098" distance-name="Half marathon"></distance-result>
+      <distance-result :seconds="seconds" distance="42.195" distance-name="Full marathon"></distance-result>
     </div>
   </div>
 </template>
@@ -45,16 +45,16 @@ body {
   height: 100%;
 }
 
-#app {
+.main {
   margin-top: -100px;
+  width: 100%;
   max-width: 600px;
   font-family: Helvetica, sans-serif;
   text-align: center;
 }
 
-.logo {
-  width: 100px;
-  height: 100px
+.distance-results {
+  //text-align: left;
 }
 
 input[type=range] {
